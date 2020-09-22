89823316da3b2ffcdc4609aff8d284c4cdb09d15
wangzengkai <wangzengkai@ppdai.com>
2020-09-22 21:12:39 +0800

    tests passed! note: Cell should add Debug

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 #[repr(u8)]
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Cell {
     Dead = 0,
     Alive = 1,
@@ -65,6 +65,11 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
+                // if js_sys::Math::random() < 0.5 {
+                //     Cell::Alive
+                // } else {
+                //     Cell::Dead
+                // }
                 if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
                 } else {
@@ -84,6 +89,30 @@ impl Universe {
         self.to_string()
     }
 
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+}
+
+impl Universe {
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
@@ -105,16 +134,15 @@ impl Universe {
         count
     }
 
-    pub fn width(&self) -> u32 {
-        self.width
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
     }
 
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
